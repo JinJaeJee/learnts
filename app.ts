6fde070e1f04ad334bef07efc3d320ef4bdd8156
JinJaeJee <jpluemphiromnat@gmail.com>
2023-10-13 17:02:36 +0700

    feat day3

--- app.ts
+++ app.ts
@@ -1,30 +1,23 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
 
-function combine(
-  input1: Combinable,
-  input2: Combinable,
-  resultConversion: ConversionDescriptor
-) {
-  let result;
-  if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  return result;
-  // if (resultConversion === 'as-number') {
-  //   return +result;
-  // } else {
-  //   return result.toString();
-  // }
+
+
+function addHandle(n1: number, n2: number, cb:(num:number) => void){
+    const result = n1+n2
+    cb(n1)
 }
 
-const combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
 
-const combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
+addHandle(10,20, (re)=> {
+    console.log(re)
+})
 
-const combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+
+function sendRequest(data: string, cb: (response: any) => void) {
+    // ... sending a request with "data"
+    return cb({data: 'Hi there!'});
+  }
+   
+  sendRequest('Send this!', (response) => { 
+    console.log(response);
+    return true;
+   });
