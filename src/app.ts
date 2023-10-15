dab814214c20ec14a902a0ed1391db72f8969cbe
JinJaeJee <jpluemphiromnat@gmail.com>
2023-10-15 19:39:56 +0700

    feat : day5

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,42 @@
 console.log("hi")
+
+class Department {
+
+    private employeeName: string[] = []
+
+    constructor (public readonly dname:string,private id:number) {
+    }
+    popout (this: Department) {
+        console.log("Show Department" + this.dname + "ID"+ this.id)
+
+    }
+
+    addEmployees(Ename:string){
+        this.employeeName.push(Ename)
+    }
+    Printoutemployee(){
+        console.log(this.employeeName)
+        console.log(this.employeeName.length)
+    }
+
+}
+//// Add new obj
+const Accounting = new Department("Accounting", 0)
+const Cleaning = new Department("Cleaning",1)
+
+// XXX.popout()
+
+// console.log(XXX)
+
+
+
+console.log(Accounting)
+console.log(Cleaning)
+
+////use method on class in individual obj
+Accounting.addEmployees("max")
+Accounting.addEmployees("jab")
+Cleaning.addEmployees("mosss")
+
+Cleaning.Printoutemployee()
+Accounting.Printoutemployee()
